e4864b8c662645b5f4e87a2b9936977a94533398
xaviguasch <xaviguasch@gmail.com>
2020-01-08 12:58:52 +0100

    got the users from an http request

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import Navbar from './layout/Navbar'
 import Users from './users/Users'
+import axios from 'axios'
 import './App.css'
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+
+    const res = await axios.get('https://api.github.com/users')
+
+    this.setState({ users: res.data, loading: false })
+
+    console.log(this.state.users)
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     )

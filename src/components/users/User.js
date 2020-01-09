85d1bb2532e89b4199b4560e10115bb2b1d8c246
xaviguasch <xaviguasch@gmail.com>
2020-01-09 23:09:41 +0100

    finished Repos component logic

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from 'react'
 import Spinner from '../layout/Spinner'
+import Repos from '../repos/Repos'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
@@ -9,10 +10,11 @@ class User extends Component {
     this.props.getUserRepos(this.props.match.params.login)
   }
 
-  static propTytpes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object,
-    getUser: PropTypes.func,
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
     getUserRepos: PropTypes.func.isRequired
   }
 
@@ -33,7 +35,7 @@ class User extends Component {
       hireable
     } = this.props.user
 
-    const { loading } = this.props
+    const { loading, repos } = this.props
 
     if (loading) return <Spinner />
 
@@ -100,6 +102,7 @@ class User extends Component {
           <div className='badge badge-light'>Public Repos: {public_repos}</div>
           <div className='badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     )
   }

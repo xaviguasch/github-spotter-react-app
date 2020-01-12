17a757c658f3e4d1cc90e52fcf6b9b0a5f6e16f5
xaviguasch <xaviguasch@gmail.com>
2020-01-12 19:35:53 +0100

    added Home component

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import User from './components/users/User'
+import Alert from './components/layout/Alert'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import GithubState from './context/github/githubState'
+import AlertState from './context/alert/alertState'
+import './App.css'
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  )
+}
+
+export default App

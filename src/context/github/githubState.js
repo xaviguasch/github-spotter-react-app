fd1b6e76c656636cb52aabcf8ca4d6cb13276962
xaviguasch <xaviguasch@gmail.com>
2020-01-13 09:55:16 +0100

    prepared environment keys for deployment

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -12,13 +12,24 @@ const GithubState = props => {
     loading: false
   }
 
+  let githubClientId
+  let githubClientSecret
+
+  if (process.env.NODE_ENV !== 'production') {
+    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET
+  } else {
+    githubClientId = process.env.GITHUB_CLIENT_ID
+    githubClientSecret = process.env.GITHUB_CLIENT_SECRET
+  }
+
   const [state, dispatch] = useReducer(GithubReducer, initialState)
 
   // Search users
   const searchUsers = async text => {
     setLoading()
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     )
 
     dispatch({
@@ -33,7 +44,7 @@ const GithubState = props => {
     setLoading()
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
     )
 
     dispatch({
@@ -47,7 +58,7 @@ const GithubState = props => {
     setLoading()
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientSecret}`
     )
 
     dispatch({
